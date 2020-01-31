ee26306c4f3a8fdbd4f2bb1695361ded84158c2d
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-01-30 22:40:55 -0300

    Creada la interface del interactor para el registro de usuario

---
+++ app/src/main/java/com/alaisoft/loginapp/domain/interactor/signupinteractor/SignUpInteractor.kt
@@ -0,0 +1,10 @@
+package com.alaisoft.loginapp.domain.interactor.signupinteractor
+
+interface SignUpInteractor {
+    interface SignUpCallback {
+        fun onSignUpSuccess()
+        fun onSignUpFailure(errorMsg:String)
+    }
+
+    fun signUp(fullname:String, email:String, password:String, listener:SignUpInteractor)
+}
