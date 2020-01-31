1f6ea9dc43ca4227727fec3b4c0991f9f94fc0be
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-01-30 23:36:36 -0300

    Configurado el método de registro en la capa de dominio

--- app/src/main/java/com/alaisoft/loginapp/domain/interactor/signupinteractor/SignUpInteractorImpl.kt
+++ app/src/main/java/com/alaisoft/loginapp/domain/interactor/signupinteractor/SignUpInteractorImpl.kt
@@ -1,7 +1,19 @@
 package com.alaisoft.loginapp.domain.interactor.signupinteractor
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+
 class SignUpInteractorImpl:SignUpInteractor {
     override fun signUp(fullname: String,email: String,password: String,listener: SignUpInteractor.SignUpCallback) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password).addOnCompleteListener {itSignUp ->
+            if(itSignUp.isSuccessful){
+                val profileUpdates = UserProfileChangeRequest.Builder().setDisplayName(fullname).build()
+                FirebaseAuth.getInstance().currentUser?.updateProfile(profileUpdates)?.addOnCompleteListener {
+                    listener.onSignUpSuccess()
+                }
+            }else{
+                listener.onSignUpFailure(itSignUp.exception?.message.toString())
+            }
+        }//FirebaseAuth listener
+    }//signUp()
 }
