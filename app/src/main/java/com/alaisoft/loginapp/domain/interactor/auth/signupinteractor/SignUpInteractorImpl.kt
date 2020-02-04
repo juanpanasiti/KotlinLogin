b79d53a16ee2e8f9ca9b722a3b9272f07c6f79e3
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-02-03 23:56:38 -0300

    Corregido el problema de displayName(), falta meterlo en el interactor

--- app/src/main/java/com/alaisoft/loginapp/domain/interactor/auth/signupinteractor/SignUpInteractorImpl.kt
+++ app/src/main/java/com/alaisoft/loginapp/domain/interactor/auth/signupinteractor/SignUpInteractorImpl.kt
@@ -2,6 +2,7 @@ package com.alaisoft.loginapp.domain.interactor.auth.signupinteractor
 
 import com.alaisoft.loginapp.presentation.auth.signup.exceptions.FirebaseSignUpException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
@@ -11,7 +12,11 @@ class SignUpInteractorImpl:SignUpInteractor {
 
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password).addOnCompleteListener {
             if(it.isSuccessful){
-                continuation.resume(Unit)
+
+                val profileUpdates = UserProfileChangeRequest.Builder().setDisplayName(fullname).build()
+                FirebaseAuth.getInstance().currentUser?.updateProfile(profileUpdates)?.addOnCompleteListener {
+                    continuation.resume(Unit)
+                }
             }else{
                 continuation.resumeWithException(FirebaseSignUpException(it.exception?.message))
             }
